5545f6a6161c7e01303c743d522094318b06fe0f
pablot95 <pablo.travi95@gmail.com>
2025-12-18 17:34:26 -0300

    Fix: Update ID mapping for 'sconcito-salmon' and bump cache version to v7

--- firebase-product-loader.js
+++ firebase-product-loader.js
@@ -336,7 +336,7 @@ const MAPEO_IDS = {
     'lomo-morron': 'LOMO_Y_MORRON',
     'verduritas-asadas': 'VERDURITAS_ASADAS',
     'medialunitas-jyq': 'MEDIALUNITAS_J_Y_Q',
-    'sconcito-salmon': 'SCONCITO_CON_CAJA_DE_SALMON',
+    'sconcito-salmon': 'SCONCITO_CON_LAJAS_DE_SALMON',
     'chipacitos-morron-panceta': 'CHIPACITOS_CON_MORRON_Y_PANCETA',
 
     // tortasClasicas
